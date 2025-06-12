32fda02af45ce82a23b855e3a7c5fd6220f8c7b0
nabil <nabilfadillah48@gmail.com>
2025-06-12 11:21:13 +0700

    1.1 - 2.2

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,25 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
-router.get('/movies', async (ctx) => {
-  return 'my awsome movies'
-})
+router.on('/').render('pages/home').as('home')
+
+// linking route
+router.get('/movies/my-awsome-movie', async (ctx) => {
+  // ctx.view.share({movie:'my awsome movie'})
+  return ctx.view.render('pages/movies',{movie:'another my awsome movie'})
+}).as('movies.show')
+
+//normal route
+// router.get('/movies', async (ctx) => {
+//   // ctx.view.share({movie:'my awsome movie'})
+//   return ctx.view.render('pages/movies',{movie:'another my awsome movie'})
+// }).as('movies.show')
+
+//resource route
+// router.get('/movies', () => {}).as('movies.index')
+// router.get('/movies/my-awesome-movie', () => {}).as('movies.show')
+// router.get('/movies/create', () => {}).as('movies create')
+// router.post('/movies', () => {}).as('movies.store')
+// router.get('/movies/my-awesome-movie/edit', () => {}).as('movies.edit')
+// router.put('/movies/my-awesome-movie', () => {}).as('movies.update')
+// router.delete('/movies/my-avesome-movie', () => {}).as('movies destroy')
