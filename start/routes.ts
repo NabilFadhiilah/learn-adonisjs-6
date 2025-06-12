cc37b3bf52f24acefb523a99e3f66a9b901df7a0
nabil <nabilfadillah48@gmail.com>
2025-06-12 17:21:55 +0700

    🍻 route validation

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,7 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
 import fs from 'node:fs/promises'
@@ -17,9 +18,20 @@ router.on('/').render('pages/home').as('home')
 router.get('/movies/:slug', async (ctx) => {
   // ctx.view.share({movie:'my awsome movie'})
   const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
-  const movie = await fs.readFile(url, 'utf8')
-  return ctx.view.render('pages/movies/show',{movie})
-}).as('movies.show')
+
+  try {
+    const movie = await fs.readFile(url, 'utf8')
+    ctx.view.share({movie})
+  } catch (error) {
+    throw new Exception(`could not find the movie ${ctx.params.slug}`,
+      {
+        code: 'E_NOT_FOUND',
+        status: 404
+      }
+    )
+  }
+  return ctx.view.render('pages/movies/show')
+}).as('movies.show').where('slug',router.matchers.slug( ))
 
 //normal route
 // router.get('/movies', async (ctx) => {
