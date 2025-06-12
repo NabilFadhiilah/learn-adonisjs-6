7de6722b17d6b6b972e430d53f0a11e29eb47e5a
nabil <nabilfadillah48@gmail.com>
2025-06-12 23:31:42 +0700

    🍻 2.11 model

--- app/services/movie_service.ts
+++ app/services/movie_service.ts
@@ -1,5 +1,5 @@
 import app from "@adonisjs/core/services/app"
-import fs from 'node:fs/promises'
+import { readdir, readFile } from 'node:fs/promises'
 import { MarkdownFile } from "@dimerapp/markdown"
 import { Exception } from "@adonisjs/core/exceptions"
 
@@ -8,30 +8,28 @@ export class MovieService {
     if (!slug.endsWith('.md')) {
       slug += '.md'
     }
-
     return app.makeURL(`resources/movies/${slug}`)
   }
 
-  static async getSlugs(){
-    const files = await fs.readdir(app.makeURL('resources/movies'))
-    return files.map((file => file.replace('.md','')))
+  static async getSlugs() {
+    const files = await readdir(app.makeURL('resources/movies'))
+    return files.map((file) => file.replace('.md', ''))
   }
 
-  static async read(slug: any){
-    try{
-
+  static async read(slug: string) {
+    try {
       const url = this.getSlugUrl(slug)
-      const file = await fs.readFile(url, 'utf8')
+      const file = await readFile(url, 'utf8')
       const md = new MarkdownFile(file)
+
       await md.process()
+
       return md
     } catch (error) {
-      throw new Exception(`could not find the movie ${slug}`,
-        {
-          code: 'E_NOT_FOUND',
-          status: 404
-        }
-      )
+      throw new Exception(`Could not find a movie called ${slug}`, {
+        code: 'E_NOT_FOUND',
+        status: 404,
+      })
     }
   }
 }
