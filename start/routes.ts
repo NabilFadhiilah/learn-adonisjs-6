00bb63f303ebffec461b2c01318c3dafb2964e9d
nabil <nabilfadillah48@gmail.com>
2025-06-13 14:38:31 +0700

    🍻 2.15 redis flush

--- start/routes.ts
+++ start/routes.ts
@@ -9,12 +9,12 @@
 
 import router from '@adonisjs/core/services/router'
 import MoviesController from '#controllers/movies_controller';
+import RedisController from '#controllers/redis_controller';
 
 router.get('/', [MoviesController,'index']).as('home')
-
-// linking route
 router.get('/movies/:slug', [MoviesController,'show']).as('movies.show').where('slug',router.matchers.slug( ))
-
+router.delete('/redis/flush',[RedisController,'flush']).as('redis.flush')
+router.delete('/redis/:slug',[RedisController,'destroy']).as('redis.destroy')
 //normal route
 // router.get('/movies', async (ctx) => {
 //   // ctx.view.share({movie:'my awsome movie'})
