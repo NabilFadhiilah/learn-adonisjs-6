3d7ce58e4b6db1966caf67dc20658dd68d551ff9
nabil <nabilfadillah48@gmail.com>
2025-06-22 12:18:51 +0700

    🍻 faker factories

---
+++ database/seeders/fake_seeder.ts
@@ -0,0 +1,14 @@
+import { CineastFactory } from '#database/factories/cineast_factory'
+import { MovieFactory } from '#database/factories/movie_factory'
+import { UserFactory } from '#database/factories/user_factory'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  static environment = ['development']
+  async run() {
+    // Write your database queries inside the run method
+    await CineastFactory.createMany(10)
+    await MovieFactory.createMany(5)
+    await UserFactory.createMany(5)
+  }
+}
