e76007cc3c73c12589ac4cda795d8454e13a0d43
nabil <nabilfadillah48@gmail.com>
2025-07-01 20:25:35 +0700

    🍻 has many on models

--- app/models/movie.ts
+++ app/models/movie.ts
@@ -1,7 +1,10 @@
 import MovieStatuses from '#enums/movie_statuses'
-import { BaseModel, beforeCreate, column, scope } from '@adonisjs/lucid/orm'
+import { BaseModel, beforeCreate, belongsTo, column, scope } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 import string from '@adonisjs/core/helpers/string'
+import MovieStatus from './movie_statues.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Cineast from './cineast.js'
 
 
 export default class Movie extends BaseModel {
@@ -40,6 +43,21 @@ export default class Movie extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @belongsTo(()=> MovieStatus,{
+    foreignKey: 'statusId',
+  })
+  declare status: BelongsTo<typeof MovieStatus>
+
+  @belongsTo(()=> Cineast,{
+    foreignKey: 'directorId',
+  })
+  declare director: BelongsTo<typeof Cineast>
+
+  @belongsTo(()=> Cineast,{
+    foreignKey: 'writerId',
+  })
+  declare writer: BelongsTo<typeof Cineast>
 
   static released = scope((query) => {
       query.where((group) =>
