990efa5a30e71ea049b80c5d8d4ab8668f1c0508
nabil <nabilfadillah48@gmail.com>
2025-07-02 23:21:49 +0700

    🍻 add validator for auth

--- app/controllers/auth/register_controller.ts
+++ app/controllers/auth/register_controller.ts
@@ -1,3 +1,5 @@
+import User from '#models/user'
+import { registerValidator } from '#validators/auth'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class RegisterController {
@@ -8,7 +10,8 @@ export default class RegisterController {
   async store({ request, response }: HttpContext) {
     const data = request.only(['fullName', 'email', 'password'])
 
-    console.log({ data })
+    const validateData = await registerValidator.validate(data)
+    const user = await User.create(validateData)
 
     return response.redirect().toRoute('home')
   }
